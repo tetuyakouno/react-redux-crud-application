711e9b4bf94f376b3868581b1e2934f1adeeb0e3
vagrant <vagrant@vagrant.vm>
2021-04-06 23:02:25 +0000

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-// import React, { Component } from "react";
-
-function App() {
-  return (<h1>Hello, world!</h1>);
-}
+import React, { Component } from "react";
 
 // function App() {
-//   return React.createElement(
-//     "div",
-//     null,
-//     "Hello, world!!!"
-//   );
+//   return (<h1>Hello, world!</h1>);
 // }
+
+class App extends Component {
+  render(){
+    // const greeting = "Hi, Tom!"
+    // const dom = <h1>{greeting}</h1>;
+    // return dom;
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    )
+  }
+}
 
 
 export default App;
