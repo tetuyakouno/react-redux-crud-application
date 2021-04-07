f0746fea9c57ab2e523eb3337181cc4ee5e223e8
vagrant <vagrant@vagrant.vm>
2021-04-07 07:42:45 +0000

    learn props of component

--- src/App.js
+++ src/App.js
@@ -19,17 +19,29 @@ import React from "react";
 // }
 
 const App = () => {
+  const profiles = [
+    { name: "taro", age: 11},
+    { name: "hanako", age: 6},
+    { }
+  ]
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
+}
+
+User.defaultProps = {
+  name: "N/A" , 
+  age: 0
 }
 
 export default App;
