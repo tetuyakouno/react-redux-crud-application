b4c28993d49a53d862a3768cbdf897fc92272532
vagrant <vagrant@vagrant.vm>
2021-04-08 01:01:11 +0000

    fix typo for components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css';
 import reducer from './reducers'
-import App from './componets/App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer)
