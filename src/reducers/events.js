a9589274f58911030e978d14b6519adeab5d7226
vagrant <vagrant@vagrant.vm>
2021-04-13 03:09:34 +0000

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,18 +1,24 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import { 
+    CREATE_EVENT,
+    READ_EVENTS, 
+    READ_EVENT, 
+    UPDATE_EVENT,
+    DELETE_EVENT,
+ } from '../actions'
 
 const switchEvents = (events = {}, action ) => {
     switch(action.type) {
         case READ_EVENTS:
-            // [
-            //  { "id": 1, "title": "Let's have an event 1!", "body": "This is the body for event 1."},
-            //  { "id": 2,"title": "Let's have an event 2!", "body": "This is the body for event 2."}
-            // ]
-            // {
-            //  1: { "id": 1, "title": "Let's have an event 1!", "body": "This is the body for event 1."},
-            //  2: { "id": 2,"title": "Let's have an event 2!", "body": "This is the body for event 2."}
-            // }
             return _.mapKeys(action.response.data, 'id')
+        case CREATE_EVENT:
+        case UPDATE_EVENT:
+        case READ_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
+        case DELETE_EVENT:
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
